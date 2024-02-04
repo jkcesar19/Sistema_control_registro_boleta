f18c0e68f6575f164c65c3088282d776903e7e11
jkcesar19 <llaguentocarloscesar96@gmail.com>
2024-02-04 10:55:48 -0500

    se actualizo el proyecto usuario

--- src/entity/implement/UsuarioImplDao.java
+++ src/entity/implement/UsuarioImplDao.java
@@ -1,19 +1,14 @@
 package entity.implement;
 
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 import entity.Usuario;
 import entity.interfas.UsuarioDao;
-import java.security.MessageDigest;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
 import java.util.Vector;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 
 public class UsuarioImplDao implements UsuarioDao {
 
@@ -35,14 +30,13 @@ public class UsuarioImplDao implements UsuarioDao {
         String sql = "SELECT * FROM `vista_usuario` ";
         st = con.createStatement();
         ResultSet rs = st.executeQuery(sql);
-        while (rs.next()) {
+        while (rs.next())
+        {
             Vector usuario = new Vector();
 
             usuario.add(rs.getString("nombre"));
             usuario.add(rs.getString("usuario"));
 
-//            String encriptada = Encriptar(rs.getString("passwor"));
-//            usuario.add(encriptada);
             usuario.add(rs.getString("rol"));
             lista.add(usuario);
         }
@@ -52,7 +46,8 @@ public class UsuarioImplDao implements UsuarioDao {
     @Override
     public boolean grabar(Object object) throws SQLException {
         objUsuario = (Usuario) object;
-        try {
+        try
+        {
             String sql = "{CALL,pro_registrar_usuario(?,?,?,?)}";
             cst = con.prepareCall(sql);
             cst.setString(1, objUsuario.getUsuario());
@@ -62,7 +57,8 @@ public class UsuarioImplDao implements UsuarioDao {
             cst.execute();
             cst.close();
             return true;
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
             throw e;
         }
     }
@@ -70,14 +66,16 @@ public class UsuarioImplDao implements UsuarioDao {
     @Override
     public boolean eliminar(Object object) throws SQLException {
         objUsuario = (Usuario) object;
-        try {
+        try
+        {
             String sql = "{CALL,pro_eliminar_usuario(?)}";
             cst = con.prepareCall(sql);
             cst.setInt(1, objUsuario.getId());
             cst.execute();
             cst.close();
             return true;
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
             throw e;
         }
     }
@@ -85,7 +83,8 @@ public class UsuarioImplDao implements UsuarioDao {
     @Override
     public boolean modificar(Object object) throws SQLException {
         objUsuario = (Usuario) object;
-        try {
+        try
+        {
             String sql = "{CALL,pro_modificar_usuario(?,?,?,?,?)}";
             cst = con.prepareCall(sql);
             cst.setString(1, objUsuario.getUsuario());
@@ -96,7 +95,8 @@ public class UsuarioImplDao implements UsuarioDao {
             cst.execute();
             cst.close();
             return true;
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
             throw e;
         }
     }
@@ -106,12 +106,14 @@ public class UsuarioImplDao implements UsuarioDao {
         Usuario usuario = null;
         ResultSet rs = null;
         PreparedStatement pst = null;
-        try {
+        try
+        {
             String sql = "SELECT * FROM vista_usuario "
                     + " WHERE usuario = '" + user + "'  AND passwor = '" + clave + "'";
             pst = con.prepareStatement(sql);
             rs = pst.executeQuery(sql);
-            if (rs.next()) {
+            if (rs.next())
+            {
                 usuario = new Usuario();
                 usuario.setId(rs.getInt(1));
                 usuario.setPersona(rs.getString(2));
@@ -119,69 +121,36 @@ public class UsuarioImplDao implements UsuarioDao {
                 usuario.setPasswor(rs.getString(4));
                 usuario.setRol(rs.getString(5));
             }
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             throw e;
-        } finally {
-            if (rs != null) {
+        } finally
+        {
+            if (rs != null)
+            {
                 rs.close();
-            } else {
+            } else
+            {
                 pst.close();
             }
         }
         return usuario;
     }
 
-    // Clave de encriptación / desencriptación
-    public SecretKeySpec CrearCalve(String llave) {
-        try {
-            byte[] cadena = llave.getBytes("UTF-8");
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            cadena = md.digest(cadena);
-            cadena = Arrays.copyOf(cadena, 16);
-            SecretKeySpec secretKeySpec = new SecretKeySpec(cadena, "AES");
-            return secretKeySpec;
-        } catch (Exception e) {
-            return null;
-        }
 
-    }
-
-    // Encriptar
-    public String Encriptar(String encriptar) {
-
-        try {
-            SecretKeySpec secretKeySpec = CrearCalve(LLAVE);
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-
-            byte[] cadena = encriptar.getBytes("UTF-8");
-            byte[] encriptada = cipher.doFinal(cadena);
-            String cadena_encriptada = Base64.encode(encriptada);
-            return cadena_encriptada;
-
-        } catch (Exception e) {
-            return "";
-        }
-    }
-
-    /**
-     *
-     * @param per
-     * @param user
-     * @return
-     * @throws Exception
-     */
     @Override
-    public Usuario validarUsuarioId(String per,String user) throws Exception {
+    public Usuario validarUsuarioId(String per, String user) throws Exception {
         Usuario usuario = null;
         ResultSet rs = null;
         PreparedStatement pst = null;
-        try {
+        try
+        {
             String sql = "SELECT * FROM vista_usuario "
                     + " WHERE nombre = '" + per + "' AND usuario = '" + user + "'";
             pst = con.prepareStatement(sql);
             rs = pst.executeQuery(sql);
-            if (rs.next()) {
+            if (rs.next())
+            {
                 usuario = new Usuario();
                 usuario.setId(rs.getInt(1));
                 usuario.setPersona(rs.getString(2));
@@ -189,17 +158,20 @@ public class UsuarioImplDao implements UsuarioDao {
                 usuario.setPasswor(rs.getString(4));
                 usuario.setRol(rs.getString(5));
             }
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             throw e;
-        } finally {
-            if (rs != null) {
+        } finally
+        {
+            if (rs != null)
+            {
                 rs.close();
-            } else {
+            } else
+            {
                 pst.close();
             }
         }
         return usuario;
     }
-
 
 }
